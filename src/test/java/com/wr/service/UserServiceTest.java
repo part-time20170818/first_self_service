80545b0ef6967eec84b329bd00f86bfd7c5bb5cf
xiewenge111 <741763234@qq.com>
2017-09-02 14:22:33 +0800

    实现jenkins自动构建调试

--- src/test/java/com/wr/service/UserServiceTest.java
+++ src/test/java/com/wr/service/UserServiceTest.java
@@ -1,11 +1,15 @@
 package com.wr.service;
 
-import com.wr.baseTest.BaseJunit4Test;
 import com.wr.domain.User;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-public class UserServiceTest extends BaseJunit4Test{
+@RunWith(SpringJUnit4ClassRunner.class)  //使用junit4进行测试
+@ContextConfiguration(locations = { "classpath:application.xml","classpath:/springmvc/spring-mvc.xml"})
+public class UserServiceTest{
     @Autowired
     private UserService userService;
 	
