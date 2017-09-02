3ad7defe24ca6448398ca02a63fcb163a1c423f6
xiewenge111 <741763234@qq.com>
2017-09-02 23:08:38 +0800

    测试获取服务器ip地址,修改页面展示---兼容linux1

--- src/main/java/com/wr/controller/GatewayController.java
+++ src/main/java/com/wr/controller/GatewayController.java
@@ -41,7 +41,7 @@ public class GatewayController {
         logger.info("user:"+user);
         InetAddress address = null;
         try {
-           user.setIp(getAddress()+"");
+           user.setIp(getLocalIP());
         } catch (Exception e) {
             logger.info(e.getMessage());
 
@@ -66,5 +66,67 @@ public class GatewayController {
             //logger.debug("Error when getting host ip address: <{}>.", e.getMessage());
         }
         return null;
+    }
+    public static String getLocalIP() throws UnknownHostException, SocketException {
+        if (isWindowsOS()) {
+            return InetAddress.getLocalHost().getHostAddress();
+        } else {
+            return getLinuxLocalIp();
+        }
+    }
+
+    /**
+     * 判断操作系统是否是Windows
+     *
+     * @return
+     */
+    public static boolean isWindowsOS() {
+        boolean isWindowsOS = false;
+        String osName = System.getProperty("os.name");
+        if (osName.toLowerCase().indexOf("windows") > -1) {
+            isWindowsOS = true;
+        }
+        return isWindowsOS;
+    }
+
+    /**
+     * 获取本地Host名称
+     */
+    public static String getLocalHostName() throws UnknownHostException {
+        return InetAddress.getLocalHost().getHostName();
+    }
+
+    /**
+     * 获取Linux下的IP地址
+     *
+     * @return IP地址
+     * @throws SocketException
+     */
+    private static String getLinuxLocalIp() throws SocketException {
+        String ip = "";
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                String name = intf.getName();
+                if (!name.contains("docker") && !name.contains("lo")) {
+                    for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                        InetAddress inetAddress = enumIpAddr.nextElement();
+                        if (!inetAddress.isLoopbackAddress()) {
+                            String ipaddress = inetAddress.getHostAddress().toString();
+                            if (!ipaddress.contains("::") && !ipaddress.contains("0:0:") && !ipaddress.contains("fe80")) {
+                                ip = ipaddress;
+                                System.out.println(ipaddress);
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            System.out.println("获取ip地址异常");
+            ip = "127.0.0.1";
+            ex.printStackTrace();
+        }
+        System.out.println("IP:"+ip);
+        return ip;
     }
 }
